41352a0549b45f9bbf9014fb800cc4cc7c307130
Siddharth Kothari <sidkothari005@gmail.com>
2022-03-11 16:06:25 +0530

    todolist

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 // app.use()
 
 const app = express();
-let tasks = [];
+
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const taskSchema = {
+    name: String
+};
+
+const Task = mongoose.model("Task", taskSchema);
+
+const task1 = new Task({
+    name: "Attend GRE Class"
+});
+
+const task2 = new Task({
+    name: "Have breakfast"
+});
+
+const task3 = new Task({
+    name: "Study"
+});
+
+const defaultTasks = [task1, task2, task3];
+
+Task.insertMany(defaultTasks, function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Success.")
+    }
+
+})
+
 app.use(express.static("public"));
 app.get("/", function(req, res) {
+
     let today = new Date();
     let options_day = {
         weekday: "short",
@@ -18,10 +52,6 @@ app.get("/", function(req, res) {
         year: "numeric"
     };
 
-    // let options_time = {
-    //     hour: "numeric",
-    //     minute: "numeric"
-    // };
 
     let day = today.toLocaleDateString("en-US", options_day);
     //let time = today.toLocaleTimeString("en-US", options_time);
